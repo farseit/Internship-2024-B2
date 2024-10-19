87acd9f8debba7577a9e506b416469bcb859bd56
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-10-19 23:06:39 +0600

    update: image gallary

--- frontend/components/ProductImageGallery/ImageGallery.jsx
+++ frontend/components/ProductImageGallery/ImageGallery.jsx
@@ -5,6 +5,7 @@ import "./imageGallery.css";
 
 const ImageGallery = ({ image, productName, images }) => {
   const [showImage, setShowImage] = useState("");
+
   // image zooming
   useEffect(() => {
     let imageZoom = document.getElementById("imageZoom");
@@ -14,30 +15,31 @@ const ImageGallery = ({ image, productName, images }) => {
         x: (event.offsetX * 100) / imageZoom.offsetWidth,
         y: (event.offsetY * 100) / imageZoom.offsetHeight,
       };
-      console.log(pointer);
       imageZoom.style.setProperty("--zoomX", `${pointer.x}%`);
       imageZoom.style.setProperty("--zoomY", `${pointer.y}%`);
     });
+
     imageZoom.addEventListener("mouseout", (event) => {
       imageZoom.style.setProperty("--display", "none");
     });
   }, []);
 
   const styles = {
-    "--url": `url(${showImage ? showImage.image : image})`,
+    "--url": `url(${image})`,
     "--zoomX": "0%",
     "--zoomY": "0%",
-    "--display": `none`,
+    "--display": "none",
   };
 
   return (
     <div>
-      <div id="imageZoom" style={styles}>
+      <div id="imageZoom" style={styles} className="border-2 border-red-500">
         <Image
           src={showImage ? showImage.image : image}
           alt={productName}
           height={400}
           width={400}
+          className="inline"
         />
       </div>
       <div className="flex gap-5 mt-4">
@@ -49,7 +51,7 @@ const ImageGallery = ({ image, productName, images }) => {
               alt="Product Image"
               height={100}
               width={100}
-              className={`h-[100px] w-full flex-grow  object-cover ${
+              className={`h-[100px] w-full flex-grow object-cover ${
                 img?.id === showImage?.id &&
                 "border p-1 border-[#192a56] duration-200"
               }`}
